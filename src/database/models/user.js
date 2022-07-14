8ccac1aadfbef07669bb7bdf2cf0b6424883e395
Andre <andre360.ars@gmail.com>
2022-07-14 12:14:11 -0300

    fix(models): :bug: Fixes the model User, adding the necessary constraints to the keys

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -2,10 +2,28 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     "User",
     {
-      displayName: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      image: DataTypes.STRING
+      id: { 
+        type: DataTypes.STRING,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      displayName: { 
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: { 
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      password: { 
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      image: { 
+        type: DataTypes.STRING,
+        allowNull: true
+      },
     },
     {
       timestamps: false,
