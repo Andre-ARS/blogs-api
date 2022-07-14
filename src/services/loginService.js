34a8769138375a6d24e6f4681aa5e50a5c881c74
Andre <andre360.ars@gmail.com>
2022-07-14 00:55:12 -0300

    fix(loginService): :bug: Changes the validation from Joi to if

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,31 +1,27 @@
 const Joi = require('joi');
-const { jwtService } = require('.');
-const User = require('../database/models/user');
+const { User } = require('../database/models/index');
+const { createToken } = require('./jwtService');
 
-const validateBody = (data) => {
-  const schema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-  }).messages({
-    'any.required': 'Some required fields are missing',
-  });
-
-  const { error } = schema.validate(data);
-
-  if (error) return { message: error.message };
+const validateBody = ({ email, password }) => {
+  if (!email || !password) return { message: 'Some required fields are missing' };
 };
 
-const signIn = (userInfo) => {
-  const user = User.findOne({ where: { ...userInfo } });
+const signIn = async (userInfo) => {
+  if (validateBody(userInfo)) {
+    return { code: 400, result: validateBody(userInfo) };
+  }
+
+  const user = await User.findOne({
+    where: { email: userInfo.email, password: userInfo.password },
+  });
 
   if (!user) return { code: 400, result: { message: 'Invalid fields' } };
-  if (validateBody(userInfo)) return { code: 400, result: validateBody(userInfo) };
 
   const { password: _, ...userWithoutPassword } = user;
 
-  const token = jwtService.createToken(userWithoutPassword.dataValues);
+  const token = createToken(userWithoutPassword.dataValues);
 
-  return { code: 200, result: token };
+  return { code: 200, result: { token } };
 };
 
 module.exports = {
