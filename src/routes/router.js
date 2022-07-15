f23cdd0124453034389eae798b906f0866a57071
Andre <andre360.ars@gmail.com>
2022-07-15 09:33:11 -0300

    fix(routes): :bug: Removes unnused middleware at the routers file

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { authMiddleware } = require('../middlewares');
+// const { authMiddleware } = require('../middlewares');
 const loginRoute = require('./loginRoute');
 const userRoute = require('./userRoute');
 
@@ -7,6 +7,6 @@ const router = express.Router();
 
 router.use('/login', loginRoute);
 router.use('/user', userRoute);
-router.use(authMiddleware.validateToken);
+// router.use(authMiddleware.validateToken);
 
 module.exports = router;
