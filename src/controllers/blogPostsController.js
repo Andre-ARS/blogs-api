26a0b34bc44e2e76ff3360a45ec86b0ec6354f20
Andre <andre360.ars@gmail.com>
2022-07-18 16:20:16 -0300

    feat(blogPostsController): :sparkles: Creates the controller updatePost to edit the informations of a post
    Req15

--- src/controllers/blogPostsController.js
+++ src/controllers/blogPostsController.js
@@ -1,4 +1,4 @@
-const { blogPostsService } = require('../services');
+const { blogPostsService } = require("../services");
 
 const addPost = async (req, res) => {
   const postInfo = { ...req.body, userId: req.user.id };
@@ -16,14 +16,31 @@ const getAllPosts = async (_req, res) => {
 
 const getPostById = async (req, res) => {
   const { id } = req.params;
-  
+
   const { code, result } = await blogPostsService.getPostById(Number(id));
 
-  return res.status(code).json(result); 
+  return res.status(code).json(result);
+};
+const updatePost = async (req, res) => {
+  const {
+    params: { id },
+    body: { title, content },
+    user: { id: userId },
+  } = req;
+
+  const { code, result } = await blogPostsService.updatePost({
+    id,
+    title,
+    content,
+    userId,
+  });
+
+  return res.status(code).json(result);
 };
 
 module.exports = {
   addPost,
   getAllPosts,
   getPostById,
+  updatePost,
 };
