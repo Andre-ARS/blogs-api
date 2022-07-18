6d0069800055d693525ee4ff0df3c8614b79f86e
Andre <andre360.ars@gmail.com>
2022-07-18 16:21:34 -0300

    feat(routes): :sparkles: Creates the route PUT/post/id to edit a post
    Req15

--- src/routes/blogPostsRoute.js
+++ src/routes/blogPostsRoute.js
@@ -9,5 +9,6 @@ postsRoute.use(authMiddleware.validateToken);
 postsRoute.get('/', blogPostsController.getAllPosts);
 postsRoute.get('/:id', blogPostsController.getPostById);
 postsRoute.post('/', blogPostsController.addPost);
+postsRoute.put('/:id', blogPostsController.updatePost);
 
 module.exports = postsRoute;
